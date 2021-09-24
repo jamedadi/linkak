9019974b851105853edcbf9f9107701f8d885916
MrMohammadY <m.yazdani000@gmail.com>
2021-09-24 18:19:22 +0330

    Add(link/models): create domain model

--- app/link/models.py
+++ app/link/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from app.library.models import BaseModel
+
+
+class DomainStatus(models.IntegerChoices):
+    ALLOW = 0, _('allow')
+    BLOCK = 1, _('block')
+
+
+class Domain(BaseModel):
+    host = models.CharField(max_length=253, verbose_name=_('host'))
+    status = models.PositiveSmallIntegerField(
+        verbose_name=_('status'), choices=DomainStatus.choices, default=DomainStatus.ALLOW
+    )
+
+    class Meta:
+        verbose_name = _('Domain')
+        verbose_name_plural = _('Domains')
+        db_table = 'domain'
