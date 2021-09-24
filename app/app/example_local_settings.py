f8c3e940aa7ad94f9a74e58d55f0dc639a97b100
Mr.Rezoo <rezam578@gmail.com>
2021-09-24 15:49:15 +0330

    Add(settings): add example local settings file

---
+++ app/app/example_local_settings.py
@@ -0,0 +1,22 @@
+# django secret_key
+SECRET_KEY = ''
+
+# setting options
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+THIRD_APPS_DEBUG_MODE = []
+
+THIRD_PARTY_MIDDLEWARE = []
+
+
+# Databases settings
+
+DATABASE_NAME = ''
+DATABASE_PASSWORD = ''
+DATABASE_HOST = ''
+DATABASE_USER = ''
+DATABASE_PORT = ''
+
