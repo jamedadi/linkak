f27f1325eaae0a92c8f32453f9525fa1beb68639
MrMohammadY <m.yazdani000@gmail.com>
2021-09-25 21:09:09 +0330

    Add(link/admin): create link, domain admin model

--- app/link/admin.py
+++ app/link/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.link.models import Domain, Link
+
+
+@admin.register(Domain)
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ('host', 'status')
+    list_filter = ('status',)
+    search_fields = ('host',)
+
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    # TODO-2: add owner to list display
+    list_display = ('domain', 'slug', 'url', 'visitor_limit', 'expire_time')
+    search_fields = ('domain', 'slug', 'url', 'visitor_limit')
