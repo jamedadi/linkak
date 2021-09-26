174e9ca6d8d191d1fa6b5b442a924d46a34ec4dc
Mr.Rezoo <rezam578@gmail.com>
2021-09-26 14:14:34 +0330

    Fix(accounts): change user model attr

--- app/app/settings.py
+++ app/app/settings.py
@@ -105,6 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -127,5 +129,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'accounts.User'
